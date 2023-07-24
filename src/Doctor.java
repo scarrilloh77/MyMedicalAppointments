690adf4f85e1618f86286159d17f42199b990178
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-07-23 22:51:24 -0500

    Create Static Elements

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,21 +1,28 @@
 public class Doctor {
-    int id;
+    static int id = 0; // Autoincrement cause is static
     String name;
     String specialty;
 
     // Constructor
-    /*
+
     Doctor(){
         System.out.println("Construyendo el Object Doctor");
+        id++;
     }
-     */
 
+    /*
     Doctor(String name){
         System.out.println("El nombre del Doctor asignado es: " + name);
     }
+    */
+
 
     // Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id); // I dont call like Doctor.id cause is 'static '
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import static ui.UIMenu.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor("Felipe Meza");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Juan Carrillo";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor.id++;
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        showMenu();
     }
+
+
 }
