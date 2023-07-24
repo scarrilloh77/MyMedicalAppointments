280183f3ca34ca1d1b5919df56e85d3c3616a70c
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-07-23 22:13:49 -0500

    Constructor method

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,17 @@ public class Doctor {
     String name;
     String specialty;
 
+    // Constructor
+    /*
+    Doctor(){
+        System.out.println("Construyendo el Object Doctor");
+    }
+     */
+
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es: " + name);
+    }
+
     // Comportamientos
     public void showName(){
         System.out.println(name);
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
+
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Felipe Meza");
         myDoctor.name = "Juan Carrillo";
         myDoctor.showName();
     }
