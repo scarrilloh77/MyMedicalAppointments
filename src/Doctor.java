80ed00c8277af02d1aa37a5e8d835ee735afbc86
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-07-24 02:46:24 -0500

    Nested Classes

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     static int id = 0; // Autoincrement cause is static
-    String name;
-    String specialty;
+    private String name;
+    private String email;
+    private String specialty;
+
 
     // Constructor
 
@@ -10,7 +15,7 @@ public class Doctor {
 //    }
 
 
-    Doctor(String name, String specialty){
+    Doctor(String name, String specialty) {
         System.out.println("El nombre del Doctor asignado es: " + name);
         id++;
         this.name = name;
@@ -18,13 +23,62 @@ public class Doctor {
     }
 
 
-
     // Comportamientos
-    public void showName(){
+    public void showName() {
         System.out.println(name);
     }
 
-    public void showId(){
+    public void showId() {
         System.out.println("ID Doctor: " + id); // I dont call like Doctor.id cause is 'static '
+
+    }
+
+
+    // Available Appointment
+    public static class AvailableAppointment {
+        private int id; // Of the AvailableAppointment.
+        private Date date;
+        private String time;
+
+        //Constructor
+        public AvailableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+    }
+
+    // Appointments collection
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+
+    public void addAvailableAppointment(Date date, String time) {
+        availableAppointments.add(new Doctor.AvailableAppointment(date, time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments(){
+        return availableAppointments;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
@@ -25,21 +27,21 @@ public class Main {
         // -----------------------------------------------------------
 
         // String name = "Ann"; // OBJETO name de TIPO String.
-        int i = 0; // VARIABLE int de TIPO PRIMITIVO int.
-        int b = 2;
-        b = i;
-        // b = 0
+//        int i = 0; // VARIABLE int de TIPO PRIMITIVO int.
+//        int b = 2;
+//        b = i;
+//        // b = 0
 
-        Patient myPatient = new Patient("Maria Gutierrez", "mgutierrez@test.com");
-        Patient myPatient2 = new Patient("Camila Cantillo", "ccantillo@test.com");
-        System.out.println(myPatient.getName()); //Maria Gutierrez
-        System.out.println(myPatient2.getName()); //Camila Cantillo
-        myPatient2 = myPatient;
-        System.out.println(myPatient.getName()); //Maria Gutierrez
-        System.out.println(myPatient2.getName()); //Maria Gutierrez
-        myPatient2.setName("Carla Garcia");
-        System.out.println(myPatient.getName()); //Carla Garcia
-        System.out.println(myPatient2.getName()); //Carla Garcia
+//        Patient myPatient = new Patient("Maria Gutierrez", "mgutierrez@test.com");
+//        Patient myPatient2 = new Patient("Camila Cantillo", "ccantillo@test.com");
+//        System.out.println(myPatient.getName()); //Maria Gutierrez
+//        System.out.println(myPatient2.getName()); //Camila Cantillo
+//        myPatient2 = myPatient;
+//        System.out.println(myPatient.getName()); //Maria Gutierrez
+//        System.out.println(myPatient2.getName()); //Maria Gutierrez
+//        myPatient2.setName("Carla Garcia");
+//        System.out.println(myPatient.getName()); //Carla Garcia
+//        System.out.println(myPatient2.getName()); //Carla Garcia
 
         // -----------------------------------------------------------
 
@@ -65,5 +67,16 @@ public class Main {
         // ... 5 MONTHS LATER...
 //        myPatient.weight = 75.5;
 //        myPatient.name = "Juan"; //What!!!
+
+        // ---------------------------Nested Classes--------------------------------
+        Doctor myDoctor = new Doctor("Pedro Sanchez", "Cirgujano");
+        myDoctor.addAvailableAppointment(new Date(), "2pm");
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "6pm");
+        System.out.println(myDoctor.getAvailableAppointments());
+
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()){
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
     }
 }
